643295987d915a40188b61fa0cd02a78090170a7
balintkiraly <balint.kiraly@protonmail.com>
2020-08-12 20:01:48 +0200

    Fix migration

--- db/migrate/20200812173622_create_users.rb
+++ db/migrate/20200812173622_create_users.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :id
       t.string :first_name
       t.string :last_name
       t.string :oauth_id
