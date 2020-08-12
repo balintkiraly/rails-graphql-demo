3b3d4772eb4245fc901ca0dda128890fd5fd0919
balintkiraly <balint.kiraly@protonmail.com>
2020-08-12 20:12:00 +0200

    Add user type

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  class PostType < Types::BaseObject
+    description 'A User'
+    field :id, ID, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :oauth_id, String, null: false
+  end
+end
